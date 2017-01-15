30bfac1389f210dd79053528b25cf6beec898903
Tom Nam <tom.minwoo.nam@gmail.com>
2017-01-15 01:56:12 -0800

    changed some comments

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -11,7 +6,11 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        /**
+         * User: Tommy
+         * Date: 1/15/2017
+         * Time: 1:49 AM
+         */
         echo "Hello out there... I live!";
         ?>
     </body>
