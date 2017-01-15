17e2a14a85012135ade3be054f128d4b2725d46a
Tom Nam <tom.minwoo.nam@gmail.com>
2017-01-15 02:01:30 -0800

    Some functions were added average toString

--- Student.php
+++ Student.php
@@ -26,4 +26,22 @@ class Student
         $this->grades[] = $grade;
     }
 
+    function average()
+    {
+        $total = 0;
+        foreach ($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
+    }
+
+    function toString()
+    {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' ('.$this->average().")\n";
+        foreach($this->emails as $which=>$what)
+            $result .= $which . ': '. $what. "\n";
+        $result .= "\n";
+        return '<pre>'.$result.'</pre>';
+    }
+
 }
